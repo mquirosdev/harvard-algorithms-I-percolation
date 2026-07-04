3e1bb95cda5bcbd44a0d46ef343bd26bd7587ade
Monica Quiros <mquirosdev@gmail.com>
2026-07-04 15:17:43 -0500

    Fix linter violations

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,50 +1,57 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
-import java.util.Arrays;
-import java.util.ArrayList;
 
 public class Percolation {
-    private int size;
+    private final int size;
     private int opened;
     private boolean[][] matrix;
     private WeightedQuickUnionUF weightedQuickUnionUF;
 
     public Percolation(int n) {
-        if(n <= 0) { throw new IllegalArgumentException("n needs to be at least 1"); }
+        if (n <= 0) {
+            throw new IllegalArgumentException("n needs to be at least 1");
+        }
 
         opened = 0;
         size = n;
 
-        // Initialize percolation matrix
         matrix = new boolean[size][size];
-        for (boolean[] row : matrix) { 
-            Arrays.fill(row, false);
+        for (boolean[] row : matrix) {
+            for (int i = 0; i < row.length; i++) {
+                row[i] = false;
+            }
         }
 
-        // Initialize quick union
         weightedQuickUnionUF = new WeightedQuickUnionUF((size * size) + 2);
     }
 
     // Indexes are in base 1
     public void open(int idxRow, int idxCol) {
-        idxRow -= 1; idxCol -= 1;
+        idxRow -= 1;
+        idxCol -= 1;
         checkIndexInBounds(idxRow, idxCol);
 
         // Check if the cell is already open
         boolean cellValue = matrix[idxRow][idxCol];
-        if(cellValue) { return; }
+        if (cellValue) {
+            return;
+        }
 
         opened += 1;
         // Open the node and connect with open neighbors
         matrix[idxRow][idxCol] = true;
         int currentNode = getCellIndex(idxRow, idxCol);
-        for(int node : getConnections(idxRow, idxCol)) {
+        for (int node : getConnections(idxRow, idxCol)) {
+            if (node == -1) {
+                continue;
+            }
             weightedQuickUnionUF.union(currentNode, node);
         }
     }
 
     // Indexes are in base 1
     public boolean isOpen(int idxRow, int idxCol) {
-        idxRow -= 1; idxCol -= 1;
+        idxRow -= 1;
+        idxCol -= 1;
         checkIndexInBounds(idxRow, idxCol);
 
         return matrix[idxRow][idxCol];
@@ -52,13 +59,18 @@ public class Percolation {
 
     // Indexes are in base 1
     public boolean isFull(int idxRow, int idxCol) {
-        idxRow -= 1; idxCol -= 1;
+        idxRow -= 1;
+        idxCol -= 1;
         checkIndexInBounds(idxRow, idxCol);
 
-        if(!isOpen(idxRow, idxCol)) { return false; }
+        if (!isOpen(idxRow, idxCol)) {
+            return false;
+        }
 
         int cellIndex = getCellIndex(idxRow, idxCol);
-        return weightedQuickUnionUF.find(cellIndex) == weightedQuickUnionUF.find((size * size));
+        int cellConnectedSet = weightedQuickUnionUF.find(cellIndex);
+        int topConnectedSet = weightedQuickUnionUF.find((size * size));
+        return cellConnectedSet == topConnectedSet;
     }
 
     public int numberOfOpenSites() {
@@ -66,44 +78,76 @@ public class Percolation {
     }
 
     public boolean percolates() {
-        return weightedQuickUnionUF.find((size * size)) == weightedQuickUnionUF.find((size * size) + 1);
+        int topComponentSet = weightedQuickUnionUF.find((size * size));
+        int bottomComponentSet = weightedQuickUnionUF.find((size * size) + 1);
+        return topComponentSet == bottomComponentSet;
     }
 
     private void checkIndexInBounds(int idxRow, int idxCol) {
         boolean rowOutOfBounds = idxRow < 0 || idxRow >= matrix.length;
         boolean colOutOfBounds = idxCol < 0 || idxCol >= matrix[0].length;
-        if(rowOutOfBounds || colOutOfBounds) {
+        if (rowOutOfBounds || colOutOfBounds) {
             throw new IllegalArgumentException("row or col out of bounds");
         }
     }
 
-    private ArrayList<Integer> getConnections(int idxRow, int idxCol) {
-        ArrayList<Integer> connections = new ArrayList<Integer>();
+    private int[] getConnections(int idxRow, int idxCol) {
+        int[] connections = new int[6];
 
         // Check the open neighbors to connect them
         int[][] neighbors = {
-                {idxRow + 1, idxCol},
-                {idxRow - 1, idxCol},
-                {idxRow, idxCol + 1},
-                {idxRow, idxCol - 1}
+                {
+                    idxRow + 1,
+                    idxCol
+                },
+                {
+                    idxRow - 1,
+                    idxCol
+                },
+                {
+                    idxRow,
+                    idxCol + 1
+                },
+                {
+                    idxRow,
+                    idxCol - 1
+                }
         };
 
-        for(int[] neighbor : neighbors) {
+        for (int i = 0; i < neighbors.length; i++) {
+            int[] neighbor = neighbors[i];
             // Check that the neighbor is not out of bounds
-            if(neighbor[0] < 0 || neighbor[0] >= matrix.length) { continue; }
-            if(neighbor[1] < 0 || neighbor[1] >= matrix[0].length) { continue; }
+            if (neighbor[0] < 0 || neighbor[0] >= matrix.length) {
+                connections[i] = -1;
+                continue;
+            }
 
-            if(matrix[neighbor[0]][neighbor[1]]) {
+            if (neighbor[1] < 0 || neighbor[1] >= matrix[0].length) {
+                connections[i] = -1;
+                continue;
+            }
+
+            if (matrix[neighbor[0]][neighbor[1]]) {
                 int nodeNumber = getCellIndex(neighbor[0], neighbor[1]);
-                connections.add(nodeNumber);
+                connections[i] = nodeNumber;
+            } else {
+                connections[i] = -1;
             }
         }
 
         // Check if it should be connected to the top node
-        if(idxRow == 0) { connections.add((size * size)); }
+        if (idxRow == 0) {
+            connections[4] = size * size;
+        } else {
+            connections[4] = -1;
+        }
 
         // Check if it should be connected to the bottom node
-        if(idxRow == matrix.length - 1) { connections.add((size * size) + 1); }
+        if (idxRow == matrix.length - 1) {
+            connections[5] = (size * size) + 1;
+        } else {
+            connections[5] = -1;
+        }
 
         return connections;
     }
--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -1,15 +1,16 @@
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
-
 public class PercolationStats {
     private int size;
     private double[] trialResults;
     private int totalTrials;
 
     public PercolationStats(int n, int trials) {
-        if(n <= 0 || trials <= 0) { throw new IllegalArgumentException("n and trials need to be at least 1"); }
-        
+        if (n <= 0 || trials <= 0) {
+            throw new IllegalArgumentException();
+        }
+
         size = n;
         totalTrials = trials;
         trialResults = new double[trials];
@@ -40,21 +41,22 @@ public class PercolationStats {
         System.out.println("mean                    = " + ps.mean());
         System.out.println("stddev                  = " + ps.stddev());
         System.out.println(
-            "95% confidence interval = [" + 
-            ps.confidenceLo() + ", " +
-            ps.confidenceHi() + "]"
+            "95% confidence interval = ["
+            + ps.confidenceLo() + ", "
+            + ps.confidenceHi() + "]"
         );
     }
 
     private void executeTrials() {
-        for(int i = 0; i < trialResults.length; i++) {
+        for (int i = 0; i < trialResults.length; i++) {
             Percolation percolation = new Percolation(size);
-            while(!percolation.percolates()) {
+            while (!percolation.percolates()) {
                 int rowIdx = StdRandom.uniformInt(1, size + 1);
                 int colIdx = StdRandom.uniformInt(1, size + 1);
                 percolation.open(rowIdx, colIdx);
             }
-            trialResults[i] = (double) percolation.numberOfOpenSites() / (size * size);
+            double sitesOpened = (double) percolation.numberOfOpenSites();
+            trialResults[i] = sitesOpened / (size * size);
         }
     }
 }

30f7e79dedac3d9eb44c7eef0dc8ab6b1966fd11
Monica Quiros <mquirosdev@gmail.com>
2026-07-05 11:27:58 -0500

    Percolation refactoring

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # harvard-algorithms-I-percolation
+
+## Requirements
+
+(Setup)[https://lift.cs.princeton.edu/java/mac/]
+
+## Specification
+
+[Specification](https://coursera.cs.princeton.edu/algs4/assignments/percolation/specification.php)
+
+## Run locally
+
+In the src folder run the following commands
+```
+javac-algs4 *.java
+java-algs4 Percolation
+java-algs4 PercolationStats
+```
--- src/Percolation.java
+++ src/Percolation.java
@@ -1,158 +1,142 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-    private final int size;
-    private int opened;
+    private int size;
     private boolean[][] matrix;
     private WeightedQuickUnionUF weightedQuickUnionUF;
+    private int openCells;
+    private int virtualTop;
+    private int virtualBottom;
 
     public Percolation(int n) {
         if (n <= 0) {
-            throw new IllegalArgumentException("n needs to be at least 1");
+            throw new IllegalArgumentException();
         }
 
-        opened = 0;
         size = n;
-
         matrix = new boolean[size][size];
-        for (boolean[] row : matrix) {
-            for (int i = 0; i < row.length; i++) {
-                row[i] = false;
-            }
-        }
-
         weightedQuickUnionUF = new WeightedQuickUnionUF((size * size) + 2);
+
+        openCells = 0;
+        virtualTop = (size * size);
+        virtualBottom = virtualTop + 1;
     }
 
-    // Indexes are in base 1
-    public void open(int idxRow, int idxCol) {
-        idxRow -= 1;
-        idxCol -= 1;
-        checkIndexInBounds(idxRow, idxCol);
+    public void open(int row, int col) {
+        int idxRow = row - 1;
+        int idxCol = col - 1;
+        isIndexInBounds(idxRow, idxCol, true);
 
-        // Check if the cell is already open
-        boolean cellValue = matrix[idxRow][idxCol];
-        if (cellValue) {
+
+        if (matrix[idxRow][idxCol]) {
             return;
         }
 
-        opened += 1;
-        // Open the node and connect with open neighbors
         matrix[idxRow][idxCol] = true;
-        int currentNode = getCellIndex(idxRow, idxCol);
-        for (int node : getConnections(idxRow, idxCol)) {
-            if (node == -1) {
-                continue;
-            }
-            weightedQuickUnionUF.union(currentNode, node);
+        openCells += 1;
+
+        if (idxRow == 0) {
+            weightedQuickUnionUF.union(
+                getFlatIndex(idxRow, idxCol),
+                virtualTop
+            );
+        }
+
+        if (idxRow == size - 1) {
+            weightedQuickUnionUF.union(
+                getFlatIndex(idxRow, idxCol),
+                virtualBottom
+            );
+        }
+
+        boolean openTop = isIndexInBounds(idxRow - 1, idxCol, false);
+        if (openTop && matrix[idxRow - 1][idxCol]) {
+            weightedQuickUnionUF.union(
+                getFlatIndex(idxRow, idxCol),
+                getFlatIndex(idxRow - 1, idxCol)
+            );
+        }
+
+        boolean openBottom = isIndexInBounds(idxRow + 1, idxCol, false);
+        if (openBottom && matrix[idxRow + 1][idxCol]) {
+            weightedQuickUnionUF.union(
+                getFlatIndex(idxRow, idxCol),
+                getFlatIndex(idxRow + 1, idxCol)
+            );
+        }
+
+        boolean openLeft = isIndexInBounds(idxRow, idxCol - 1, false);
+        if (openLeft && matrix[idxRow][idxCol - 1]) {
+            weightedQuickUnionUF.union(
+                getFlatIndex(idxRow, idxCol),
+                getFlatIndex(idxRow, idxCol - 1)
+            );
+        }
+
+        boolean openRight = isIndexInBounds(idxRow, idxCol + 1, false);
+        if (openRight && matrix[idxRow][idxCol + 1]) {
+            weightedQuickUnionUF.union(
+                getFlatIndex(idxRow, idxCol),
+                getFlatIndex(idxRow, idxCol + 1)
+            );
         }
     }
 
-    // Indexes are in base 1
-    public boolean isOpen(int idxRow, int idxCol) {
-        idxRow -= 1;
-        idxCol -= 1;
-        checkIndexInBounds(idxRow, idxCol);
+    public boolean isOpen(int row, int col) {
+        int idxRow = row - 1;
+        int idxCol = col - 1;
+        isIndexInBounds(idxRow, idxCol, true);
 
         return matrix[idxRow][idxCol];
     }
 
-    // Indexes are in base 1
-    public boolean isFull(int idxRow, int idxCol) {
-        idxRow -= 1;
-        idxCol -= 1;
-        checkIndexInBounds(idxRow, idxCol);
+    public boolean isFull(int row, int col) {
+        int idxRow = row - 1;
+        int idxCol = col - 1;
+        isIndexInBounds(idxRow, idxCol, true);
 
-        if (!isOpen(idxRow, idxCol)) {
-            return false;
-        }
+        int topComponent = weightedQuickUnionUF.find(virtualTop);
+        int component = weightedQuickUnionUF.find(
+            getFlatIndex(idxRow, idxCol)
+        );
 
-        int cellIndex = getCellIndex(idxRow, idxCol);
-        int cellConnectedSet = weightedQuickUnionUF.find(cellIndex);
-        int topConnectedSet = weightedQuickUnionUF.find((size * size));
-        return cellConnectedSet == topConnectedSet;
+        return topComponent == component;
     }
 
     public int numberOfOpenSites() {
-        return opened;
+        return openCells;
     }
 
     public boolean percolates() {
-        int topComponentSet = weightedQuickUnionUF.find((size * size));
-        int bottomComponentSet = weightedQuickUnionUF.find((size * size) + 1);
-        return topComponentSet == bottomComponentSet;
+        int topComponent = weightedQuickUnionUF.find(virtualTop);
+        int bottomComponent = weightedQuickUnionUF.find(virtualBottom);
+        return topComponent == bottomComponent;
     }
 
-    private void checkIndexInBounds(int idxRow, int idxCol) {
-        boolean rowOutOfBounds = idxRow < 0 || idxRow >= matrix.length;
-        boolean colOutOfBounds = idxCol < 0 || idxCol >= matrix[0].length;
-        if (rowOutOfBounds || colOutOfBounds) {
-            throw new IllegalArgumentException("row or col out of bounds");
+    // public static void main() {
+    //     Percolation percolation = new Percolation(10);
+
+    //     percolation.open(-1, 5);
+    // }
+
+    private boolean isIndexInBounds(
+        int idxRow,
+        int idxCol,
+        boolean raiseException
+    ) {
+        boolean inBounds = idxRow >= 0
+            && idxRow < size
+            && idxCol >= 0
+            && idxCol < size;
+
+        if (raiseException && !inBounds) {
+            throw new IllegalArgumentException("Index is out of bounds");
         }
+
+        return inBounds;
     }
 
-    private int[] getConnections(int idxRow, int idxCol) {
-        int[] connections = new int[6];
-
-        // Check the open neighbors to connect them
-        int[][] neighbors = {
-                {
-                    idxRow + 1,
-                    idxCol
-                },
-                {
-                    idxRow - 1,
-                    idxCol
-                },
-                {
-                    idxRow,
-                    idxCol + 1
-                },
-                {
-                    idxRow,
-                    idxCol - 1
-                }
-        };
-
-        for (int i = 0; i < neighbors.length; i++) {
-            int[] neighbor = neighbors[i];
-            // Check that the neighbor is not out of bounds
-            if (neighbor[0] < 0 || neighbor[0] >= matrix.length) {
-                connections[i] = -1;
-                continue;
-            }
-
-            if (neighbor[1] < 0 || neighbor[1] >= matrix[0].length) {
-                connections[i] = -1;
-                continue;
-            }
-
-            if (matrix[neighbor[0]][neighbor[1]]) {
-                int nodeNumber = getCellIndex(neighbor[0], neighbor[1]);
-                connections[i] = nodeNumber;
-            } else {
-                connections[i] = -1;
-            }
-        }
-
-        // Check if it should be connected to the top node
-        if (idxRow == 0) {
-            connections[4] = size * size;
-        } else {
-            connections[4] = -1;
-        }
-
-        // Check if it should be connected to the bottom node
-        if (idxRow == matrix.length - 1) {
-            connections[5] = (size * size) + 1;
-        } else {
-            connections[5] = -1;
-        }
-
-        return connections;
-    }
-
-    private int getCellIndex(int idxRow, int idxCol) {
+    private int getFlatIndex(int idxRow, int idxCol) {
         return (idxRow * size) + idxCol;
     }
 }
